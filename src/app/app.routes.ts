082fc8c9d1fb51a0670fa7a371baeb6c1c675783
Paulo Catto <paulohcatto@hotmail.com>
2025-10-29 21:59:10 -0300

    chore(app): setup routing, add router-outlet and global styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { SignupComponent } from './features/auth/signup/signup.component';
+import { ForgotPasswordComponent } from './features/auth/forgot-password/forgot-password.component';
+import { ChatComponent } from './features/chat/chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'chat', component: ChatComponent },
+  { path: '**', redirectTo: 'login' },
+];
