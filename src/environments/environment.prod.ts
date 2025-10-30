76da57657d333b3641fc81cfbdbda99926d51af2
Paulo Catto <paulohcatto@hotmail.com>
2025-10-29 22:29:51 -0300

    chore: add .env to gitignore to secure sensitive keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
+import { config } from 'dotenv';
+config();
+
 export const environment = {
   production: true,
-  supabaseUrl: 'https://yqtumlcvvhmmmaamlcay.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxdHVtbGN2dmhtbW1hYW1sY2F5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3NzczOTAsImV4cCI6MjA3NzM1MzM5MH0.ZmjGafui4io8yx-82XKi72STcDk8B0E_xd_1UYNd4-Y',
+  supabaseUrl: process.env['SUPABASE_URL'] || '',
+  supabaseKey: process.env['SUPABASE_ANON_KEY'] || '',
 };
