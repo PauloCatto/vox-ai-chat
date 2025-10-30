c0d70f9858f40737b120ce2480577ee7fa6a300f
Paulo Catto <paulohcatto@hotmail.com>
2025-10-29 21:58:32 -0300

    feat(chat): add layout and send message logic

--- src/app/features/chat/chat/chat.component.ts
+++ src/app/features/chat/chat/chat.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+  private fb = inject(FormBuilder);
+  chatForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.chatForm = this.fb.group({
+      message: ['', Validators.required],
+    });
+  }
+
+  sendMessage(): void {
+    if (this.chatForm.invalid) return;
+
+    const { message } = this.chatForm.value;
+    console.log('User message:', message);
+
+    this.chatForm.reset();
+  }
 }
