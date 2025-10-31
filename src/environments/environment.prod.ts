292383c880863f8fe3bdce030784979e018efa34
Paulo Catto <paulohcatto@hotmail.com>
2025-10-30 22:42:27 -0300

    chore(config): update Angular configuration and environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
-import { config } from 'dotenv';
-config();
-
 export const environment = {
   production: true,
-  supabaseUrl: process.env['SUPABASE_URL'] || '',
-  supabaseKey: process.env['SUPABASE_ANON_KEY'] || '',
+  supabaseUrl: 'https://yqtumlcvvhmmmaamlcay.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxdHVtbGN2dmhtbW1hYW1sY2F5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3NzczOTAsImV4cCI6MjA3NzM1MzM5MH0.ZmjGafui4io8yx-82XKi72STcDk8B0E_xd_1UYNd4-Y',
 };
