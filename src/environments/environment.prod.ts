09d9ec45bc9c3cab8c58e30158f680ab3c4dee57
Paulo Catto <paulohcatto@hotmail.com>
2025-11-09 15:55:06 -0300

    chore(deps): Update API keys and service URLs in environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   supabaseUrl: 'https://yqtumlcvvhmmmaamlcay.supabase.co',
   supabaseKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxdHVtbGN2dmhtbW1hYW1sY2F5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3NzczOTAsImV4cCI6MjA3NzM1MzM5MH0.ZmjGafui4io8yx-82XKi72STcDk8B0E_xd_1UYNd4-Y',
-  openAiApiKey:
-    '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',
-  chatApiUrl: 'https://api.openai.com/v1/chat/completions',
+  geminiApiKey:
+    'AIzaSyD0MGXpYMSCu6qdCyKDjX3fZW1a8DBayqI',
+  chatApiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent',
 };
