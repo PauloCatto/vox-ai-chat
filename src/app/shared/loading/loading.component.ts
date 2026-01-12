7280420eae24c922a09a326df1b426e70a6bafca
Paulo Catto <paulohcatto@hotmail.com>
2026-01-12 20:39:29 -0300

    feat(shared): create generic loading component for reuse across app

---
+++ src/app/shared/loading/loading.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.scss',
+})
+export class LoadingComponent {
+  @Input() text = 'Loading...';
+  @Input() size: 'sm' | 'md' | 'lg' = 'md';
+  @Input() fullscreen = false;
+}
